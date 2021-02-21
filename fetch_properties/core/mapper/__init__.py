553a4c89d2dc6d3cc04a78785a9c1e4db1d41c34
Alessio Vierti <alessio.vierti@gmail.com>
2021-02-21 12:55:12 +0100

    Add statistics query

--- fetch_properties/core/mapper/__init__.py
+++ fetch_properties/core/mapper/__init__.py
@@ -1,6 +1,8 @@
+import libgeohash
 from typing import Any, AnyStr, Dict, List
 
-from ..schema import Property, PropertyLocation, PropertiesPage
+from ..schema import Property, PropertyLocation, PropertiesPage, LocalStatistics, GlobalStatistics, Statistics, \
+    PriceStatistics, LocationBoundingBox, Point
 
 
 class PropertyMapper:
@@ -35,3 +37,72 @@ class PropertiesPageMapper:
         properties_page.page = properties[-1].id
 
         return properties_page
+
+
+class StatisticsMapper:
+
+    @staticmethod
+    def map(local_statistics: List[LocalStatistics], global_statistics: GlobalStatistics) -> Statistics:
+
+        mapped = Statistics()
+        mapped.local_statistics = local_statistics
+        mapped.global_statistics = global_statistics
+
+        return mapped
+
+
+class LocalStatisticsMapper:
+
+    @staticmethod
+    def map(price_statistics: PriceStatistics, geohash: AnyStr, bounding_box: LocationBoundingBox) -> LocalStatistics:
+
+        mapped = LocalStatistics()
+        mapped.price = price_statistics
+        mapped.geohash = geohash
+        mapped.bounding_box = bounding_box
+
+        return mapped
+
+
+class GlobalStatisticsMapper:
+
+    @staticmethod
+    def map(price_statistics: PriceStatistics) -> GlobalStatistics:
+
+        mapped = GlobalStatistics()
+        mapped.price = price_statistics
+
+        return mapped
+
+
+class PriceStatisticsMapper:
+
+    @staticmethod
+    def map(min_price, max_price, avg_price) -> PriceStatistics:
+
+        mapped = PriceStatistics()
+        mapped.min = min_price
+        mapped.max = max_price
+        mapped.avg = avg_price
+
+        return mapped
+
+
+class LocationBoundingBoxMapper:
+
+    @staticmethod
+    def map(geohash: AnyStr) -> LocationBoundingBox:
+
+        reversed_geohash = libgeohash.bbox(geohash)
+
+        mapped = LocationBoundingBox()
+        top_right = Point()
+        top_right.latitude = reversed_geohash.get('n')
+        top_right.longitude = reversed_geohash.get('e')
+        mapped.top_right = top_right
+        bottom_left = Point()
+        bottom_left.latitude = reversed_geohash.get('s')
+        bottom_left.longitude = reversed_geohash.get('w')
+        mapped.bottom_left = bottom_left
+
+        return mapped
--- fetch_properties/core/schema/query.py
+++ fetch_properties/core/schema/query.py
@@ -1,7 +1,7 @@
 import graphene
 from abc import abstractmethod
 
-from . import BoundingBox, PropertiesPage, PropertyFilter
+from . import SearchBoundingBox, PropertiesPage, PropertyFilter, Statistics
 from .resolver import RESOLVER_MONGODB
 
 
@@ -9,18 +9,31 @@ class Query(graphene.ObjectType):
 
     properties_by_bounding_box_and_filter = graphene.Field(
         PropertiesPage,
-        bounding_box=graphene.Argument(BoundingBox, required=True),
+        bounding_box=graphene.Argument(SearchBoundingBox, required=True),
         filter=graphene.Argument(PropertyFilter, required=True),
         page=graphene.Argument(graphene.String, required=False, default_value='')
+    )
+
+    statistics_by_filter = graphene.Field(
+        Statistics,
+        filter=graphene.Argument(PropertyFilter, required=True)
     )
 
     @abstractmethod
     def resolve_properties_by_bounding_box_and_filter(
             self, info,
-            bounding_box: BoundingBox,
+            bounding_box: SearchBoundingBox,
             filter: PropertyFilter,
             page: graphene.String
     ) -> PropertiesPage:
+
+        pass
+
+    @abstractmethod
+    def resolve_statistics_by_filter(
+            self, info,
+            filter: PropertyFilter
+    ) -> Statistics:
 
         pass
 
@@ -29,7 +42,7 @@ class MongoDBQuery(Query):
 
     def resolve_properties_by_bounding_box_and_filter(
             self, info,
-            bounding_box: BoundingBox,
+            bounding_box: SearchBoundingBox,
             filter: PropertyFilter,
             page: graphene.String
     ) -> PropertiesPage:
@@ -38,4 +51,13 @@ class MongoDBQuery(Query):
             bounding_box=bounding_box,
             filter=filter,
             page=page
+        )
+
+    def resolve_statistics_by_filter(
+            self, info,
+            filter: PropertyFilter
+    ) -> Statistics:
+
+        return RESOLVER_MONGODB.find_statistics_by_filter(
+            filter=filter
         )
